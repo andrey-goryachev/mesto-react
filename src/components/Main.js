995092f737e730d8e7d26a179a6b934f6832cd8c
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-03-20 22:01:00 +0300

    fix: make a destructuring of the props in Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,20 +2,26 @@ import React, {useContext} from 'react';
 import Card from "./Card";
 import {CurrentUserContext} from "../contexts/CurrentUserContext";
 
-function Main(props) {
+function Main({onEditProfile,
+                onEditAvatar,
+                onAddPlace,
+                onCardClick,
+                onCardLike,
+                onCardDelete,
+                cards}) {
   const currenUser = useContext(CurrentUserContext)
 
   return (
     <main className="content">
       <section className="profile">
-        <div className="profile__avatar-wrapper" onClick={props.onEditAvatar}>
+        <div className="profile__avatar-wrapper" onClick={onEditAvatar}>
           <img src={currenUser.avatar} alt="Фото профиля" className="profile__avatar"/>
         </div>
         <div className="profile__info">
           <div className="profile__wrapper-title">
             <h1 className="profile__title">{currenUser.name}</h1>
             <button
-              onClick={props.onEditProfile}
+              onClick={onEditProfile}
               className="
                 profile__button
                 profile__button_function_edit
@@ -27,7 +33,7 @@ function Main(props) {
           <p className="profile__text">{currenUser.about}</p>
         </div>
         <button
-          onClick={props.onAddPlace}
+          onClick={onAddPlace}
           className="
             profile__button
             profile__button_function_add
@@ -39,16 +45,16 @@ function Main(props) {
       <section className="elements">
         <ul className="elements__list">
           {
-            props.cards.map((card) => (
+            cards.map((card) => (
               <li
                 className="elements__card"
                 key={card._id}
               >
                 <Card
                   card={card}
-                  onCardClick={props.onCardClick}
-                  onCardLike={props.onCardLike}
-                  onCardDelete={props.onCardDelete}
+                  onCardClick={onCardClick}
+                  onCardLike={onCardLike}
+                  onCardDelete={onCardDelete}
                 />
               </li>
             ))
